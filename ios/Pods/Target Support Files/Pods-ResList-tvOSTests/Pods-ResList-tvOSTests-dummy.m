d493a075c282584f35182d59008ac31e7ea8b901
Mert YILDIRIM <m.yildirim.gdev@gmail.com>
2020-06-09 19:05:09 +0300

    pod dosyası silindi

--- ios/Pods/Target Support Files/Pods-ResList-tvOSTests/Pods-ResList-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ResList_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ResList_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ResList/Pods-ResList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ResList : NSObject
-@end
-@implementation PodsDummy_Pods_ResList
-@end
